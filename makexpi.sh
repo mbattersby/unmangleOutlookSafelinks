00301e2c4c7f3d6fc3bfc7558aa22023dc1707df
John Pye <john.pye@anu.edu.au>
2022-01-22 20:41:02 +1100

    updated manifest, packaging script

--- makexpi.sh
+++ makexpi.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-NAME=unmangleOutlookSafelinks
-VERSION=3.0.0
-rm -Rf */*~
+echo "Creating JSON..."
+yq eval -oj src/manifest.yml > src/manifest.json
 
-ZIPFILE=${NAME}-${VERSION}.xpi
+echo "Inserting README into JSON"
+
+README=$(pandoc README.md -t mediawiki) yq e -oj -P -i '.description = strenv(README)' src/manifest.json
+echo "Reading fields..."
+
+STEM=`yq e .codename src/manifest.yml`
+VERSION=`yq e .version src/manifest.yml`
+
+echo "Creating package..."
+ZIPFILE=${STEM}-${VERSION}.xpi
 
 rm -f ${ZIPFILE}
 
 (cd src && zip -r - .) > ${ZIPFILE}
+
+echo "Removing YAML file from ZIP..."
+
+zip -d ${ZIPFILE} manifest.yml
